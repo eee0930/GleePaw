73108b16e687b1809086bf7d1f2b9b02c96d7291
lazyhysong <eee0930@naver.com>
2023-12-07 17:35:26 +0900

    feat: 로그인, 회원가입 페이지 추가

--- src/routes/auth/login.tsx
+++ src/routes/auth/login.tsx
@@ -1,3 +1,80 @@
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { FirebaseError } from 'firebase/app';
+import { Link, useNavigate } from 'react-router-dom';
+import { auth } from '../../firebase';
+import {
+  Input,
+  Switcher,
+  Title,
+  Error,
+  Wrapper,
+  Form,
+} from '../../assets/authStyles';
+
 export default function Login() {
-  return <h1>Login</h1>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('');
+    if (isLoading || email === '' || password === '') {
+      return;
+    }
+    try {
+      setIsLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>Log into Glee Paw</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          name="password"
+          value={password}
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value={isLoading ? 'Loading...' : 'Log in'} />
+      </Form>
+      {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        Don't have an account?{' '}
+        <Link to="/auth/create-account">Create One &rarr;</Link>
+      </Switcher>
+    </Wrapper>
+  );
 }
